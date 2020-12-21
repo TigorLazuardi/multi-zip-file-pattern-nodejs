6e2e5644cd1b6161296bc3b78e1924dde251c838
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2020-12-21 19:59:34 +0700

    prototype multifile zip

---
+++ index.js
@@ -0,0 +1,22 @@
+import archiver from "archiver";
+import fs from "fs";
+
+const output = fs.createWriteStream("ddd.zip");
+output.on("error", console.error);
+output.on("finish", () => console.log("done writing"));
+const archive = archiver("zip");
+const streams = [fs.createReadStream("aaa.txt"), fs.createReadStream("bbb.txt"), fs.createReadStream("ccc.txt")];
+
+streams.forEach((y, i) => {
+    archive.append(y, { name: `name${i}.txt` });
+});
+
+archive.on("finish", () => console.log("done archiving"));
+
+archive
+    .finalize()
+    .then(() => console.log("done finalizing"))
+    .catch(console.error);
+
+archive.pipe(output);
+export {};
